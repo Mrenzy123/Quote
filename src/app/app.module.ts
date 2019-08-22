478d8fcddf9d457a26600bf945218dcc58595954
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-22 18:57:17 +0200

    add more file components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
+import { QuoteDeleteComponent } from './quote-delete/quote-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteVoteComponent,
+    QuoteDeleteComponent
   ],
   imports: [
     BrowserModule,
